918fe0e2cd03488c45e613251ec74d6d8617d546
maeashley <ashleystewart8888@yahoo.com>
2017-12-14 13:56:11 -0700

    added some functionality for wrapping the html

--- main.js
+++ main.js
@@ -3,25 +3,39 @@
 /* Makes changes to the web features in Canvas */
 
 const canvas = require('canvas-wrapper'),
-    async = require('async');
+    async = require('async'),
+    cheerio = require('cheerio');
 
 module.exports = (course, stepCallback) => {
+    var $;
     course.addModuleReport('web-features-update');
 
-    //1. makeChanges to the specified files
-    function divWrap(htmlBody) {}
+    //1. makeChanges to the specified html
+    function divWrap(callback) {
+        var htmlBody = document.getElementById('main'),
+            newDiv = document.createElement('div'),
+            wrapBody = htmlBody.parentNode.appendChild(newDiv),
+            wrap = wrapper.appendChild(htmlBody);
+        newDiv.setAttribute('class', 'wrapper-div');
+        callback(wrapBody, wrap);
+    }
     var changeFunctions = [divWrap];
 
     function makeChanges(callback) {
-        //I want to specify this callback... how do I do that?
         async.waterfall(changeFunctions, function (err, results) {
+            if (err) {
+                console.log(err)
+                callback(err);
+                return;
+            }
             course.success('web-features-update', 'successfully')
-            callback(null)
+            callback(null, results)
         })
     }
     //2. Complete these changes on all the specified content
     function getPages(callbackPages) {
-        var pagesApi = '',
+        var courseId = course.info.canvasOU,
+            pagesApi = '/ap1/v1/courses/' + courseId + 'pages',
             changesMade = { /*some object here*/ };
         canvas.get(contentUrl, function (err, pages) {
             if (err) {
@@ -31,19 +45,25 @@ module.exports = (course, stepCallback) => {
             }
             makeChanges(callbackPages);
             canvas.put(pagesApi, changesMade, function (err, body) {
-                /*err handling here*/
+                if (err) {
+                    console.log(err)
+                    callback(err);
+                    return;
+                }
             });
         });
     }
 
     //3. Call everything to do all of it.
-    var everything = [pages, quizzes, files, assignments];
-
-    function doEverything(everything, function (err, result) {
+    var everything = [pages, quizzes, files, assignments]
+    async.waterfall(everything, function (err, result) {
         if (err) {
             course.throwErr('web-features-update', err)
+            callback(err);
+            return;
+        } else {
+            course.success('web-features-update', 'process complete!')
         }
-        course.success('web-features-update', 'process complete!')
     })
     stepCallback(null, course);
 };
